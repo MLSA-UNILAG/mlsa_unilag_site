827f0d7ba1d1ec8a789472cd83119b5b500c6c42
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-05-28 17:11:13 +0100

    feat: Add Footer to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import './App.css'
+import "./App.css";
+import { Footer } from "./components";
 
 function App() {
-
   return (
-    <div className='text-[5rem] font-segoe flex flex-col'>
-      <span className='font-extralight'>Testing font weight</span>
-      <span className='font-light'>Testing font weight</span>
-      <span className='font-normal'>Testing font weight</span>
-      <span className='font-medium'>Testing font weight</span>
-      <span className='font-semibold'>Testing font weight</span>
-      <span className='font-bold'>Testing font weight</span>
+    <div className="text-[5rem] font-segoe flex flex-col">
+      <span className="font-extralight">Testing font weight</span>
+      <span className="font-light">Testing font weight</span>
+      <span className="font-normal">Testing font weight</span>
+      <span className="font-medium">Testing font weight</span>
+      <span className="font-semibold">Testing font weight</span>
+      <span className="font-bold">Testing font weight</span>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
