22a1bf7db0598a70851799fe430d9feb74dde6ea
Yourgotopyromaniac <cipherx809@gmail.com>
2024-06-12 12:12:44 +0100

    hero clip-path test

---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+    return (
+        <div className='w-full h-[100vh]'>
+            <div className="w-full h-full hero-bg">
+            </div>
+        </div>
+    )
+}
+
+export default Hero
