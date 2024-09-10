c33f8d2ec678cf6842ae4c0c6283fed3adba485e
NTiger07 <falaleru@gmail.com>
2024-09-10 22:56:15 +0100

    Milestone Fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,9 +32,9 @@ const Header = () => {
 
   return (
     <>
-      <div className="MOBILE md:hidden">MOBILE HEADER IS HERE</div>
+      <div className="MOBILE absolute z-40 top-6 md:hidden">MOBILE HEADER IS HERE</div>
       <div className="hidden absolute z-40 top-6 md:flex px-[4v] w-full h-[12vh] items-center justify-between">
-        <div className="bg-purple-50 py-[1rem] pr-[1.5rem] flex items-center"><img src="/mlsa.png" alt="" /></div>
+        <div className="py-[1rem] pr-[1.5rem] flex items-center"><img src="/mlsa.png" alt="" /></div>
 
         <div className="relative bg-[#ebe7f1b2] w-[62%] text-neutral-500 flex flex-col justify-between pt-[1.2rem] rounded-lg">
 
@@ -98,7 +98,7 @@ const Header = () => {
 
         </div>
 
-        <div className="bg-purple-50 rounded-bl-[10px] py-[1rem] pl-[1.5rem]">
+        <div className="py-[1rem] pl-[1.5rem]">
           <button className="flex items-center font-medium bg-gradient-to-r from-purple-600 to-purple-700 2xs:px-[.7rem] 2xs:text-[.9rem] lg:text-[1rem] lg:px-[1.5rem] py-[1rem] rounded-[10px] text-white">
             <span>Join Community</span>
             <img src="/icons/arrow-up-right.svg" alt="" className="w-[20px]" />
